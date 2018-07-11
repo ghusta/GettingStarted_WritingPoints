eee4f2cfbae863d699103d99b51669edab4730ad
Guillaume HUSTA <guillaume.husta@gmail.com>
2018-07-11 16:35:08 +0200

    Add col 'timestamp' to transform unix time to type Timestamp

--- Timestamp_Precision.py
+++ Timestamp_Precision.py
@@ -15,6 +15,12 @@ df['time_ns'] = df['unix_time'].apply(lambda x: x * 1000000000)
 # Col 'time' dtype is int64 (check with `df.dtypes`)
 # df.head(5)
 
+# add column timestamp and convert int to Timestamp 
+# (very costly operation)
+# df['timestamp'] = df['unix_time'].apply(lambda x: pd.to_datetime(x, unit='s'))
+# cheaper operation -- UTC TZ
+df['timestamp'] = df['unix_time'].apply(lambda x: pd.Timestamp.utcfromtimestamp(x) )
+
 # export as csv
 ns_precision = df
 ns_precision.to_csv('data/BTC_ns.csv', index=False)
